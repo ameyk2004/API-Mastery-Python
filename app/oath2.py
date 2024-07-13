ca773201880dfb183e8a1f94818885f824f25ccf
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2024-07-13 09:15:20 +0530

    Adding Postman Bearer token

--- app/oath2.py
+++ app/oath2.py
@@ -1,5 +1,5 @@
 from jose import JWTError, jwt
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from . import schemas
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
@@ -10,13 +10,13 @@ oauth2_schema = OAuth2PasswordBearer(tokenUrl='login')
 
 SECRET_KEY = "2a91fd44ee2581907d59553e9c2dc7f20e18b30f0f9a1394accde7e7ee2c6b12"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 1
 
 
 def create_access_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp" : expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
