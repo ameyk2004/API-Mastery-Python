b489adbb0f37ecf2ef16f1bccc4fb3253d62ba26
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2024-07-19 08:52:07 +0530

    using Environment variables

--- app/database.py
+++ app/database.py
@@ -4,8 +4,9 @@ from psycopg2.extras import RealDictCursor
 from sqlalchemy.orm import sessionmaker
 import time
 import psycopg2
+from .config import settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Amey1234@localhost/fastapi_db"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/oath2.py
+++ app/oath2.py
@@ -3,28 +3,26 @@ from datetime import datetime, timedelta, timezone
 from . import schemas
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from .config import settings
 
 oauth2_schema = OAuth2PasswordBearer(tokenUrl='login')
 
 # openssl rand -hex 32 - generate SECRET
 
-SECRET_KEY = "2a91fd44ee2581907d59553e9c2dc7f20e18b30f0f9a1394accde7e7ee2c6b12"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 def create_access_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp" : expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
 
     return encoded_jwt
 
 def verify_token(token, credentialexception):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=settings.ALGORITHM)
         id = payload.get("id")
 
         if not id: 
