78b8bc62511330dddcec65b6727a910554b9c039
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2024-07-19 08:54:24 +0530

    using Environment variables

--- app/oath2.py
+++ app/oath2.py
@@ -14,15 +14,15 @@ oauth2_schema = OAuth2PasswordBearer(tokenUrl='login')
 def create_access_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.access_token_expire_minutes)
     to_encode.update({"exp" : expire})
-    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=settings.algorithm)
 
     return encoded_jwt
 
 def verify_token(token, credentialexception):
     try:
-        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=settings.ALGORITHM)
+        payload = jwt.decode(token, settings.secret_key, algorithms=settings.algorithm)
         id = payload.get("id")
 
         if not id: 
